128ab7f4c47f7938533c0360e63d5f05aefa3373
Diana Laura Chavez Jeronimo2 <isc18350285@gmail.com>
2022-04-07 11:32:58 -0500

    primer commit de notas

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,80 @@
+import { useState } from "react"
 
 function App() {
-  let varprueba = "titulo"
 
-const handleChangePrueba = (event) => {
-  varprueba = event.target.evalue;
+ const [inputState, setInputState] = useState({
+   titulo: "", 
+   fecha: "", 
+   nota: "",
+ });
+
+
+  const handleInputChange = (event) => {
+    setInputState({
+      ...inputState,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleClickLimpiar = ()=> {
+setInputState({titulo:"",fecha: "",nota:""});
 };
-
   return (
-    <div className="App">
-      <h3>notas</h3>
-      <label htmlFor="pruebaID">Input de prueba</label>
-      <input
-         id="pruebaID"
-         name="prueba"
-         type="text"
-         onChange={handleChangePrueba}
-         value={varprueba}
-      />
+    <div className="App container">
+    <div className="row">
+    <div classMane="col">
+      <h3>lista</h3>
     </div>
-  );
-}
+    <div className="col">
+      <h3>Notas</h3>
+      <label className ="mb-2">
+        Titulo
+      <input 
+        id = "titulo" 
+        name = "titulo" 
+        type = "text" 
+        onChange = {handleInputChange}
+        value = {inputState.titulo}
+      />
+      </label>
+      <br/>
 
+      <label className = "mb-2">
+        Fecha
+      <input 
+        id = "fecha"
+        name = "fecha" 
+        type = "text" 
+        onChange = {handleInputChange}
+        value = {inputState.fecha}
+      />
+      </label>
+      <br/>
+
+      <label className = "mb-2">
+        Nota
+      <input 
+        id = "nota" 
+        name = "nota" 
+        type = "text" 
+        onChange = {handleInputChange}
+        value = {inputState.nota}
+      />
+      </label>
+      </div>
+      </div>
+      <hr />
+      <div className= "ms-2 mt-2">
+    <button
+          type="button" 
+          className="btn btn-primary" 
+          onClick={handleClickLimpiar}
+          >
+        Limpiar
+    </button>
+
+    </div>
+</div>
+  );
+  }
 export default App;
